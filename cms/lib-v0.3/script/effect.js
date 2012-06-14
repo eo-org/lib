03b5ac9b4ab8439dff2ff3cc0cc62bb6f809a73c
Jingjing Lu <lujingjing@enorange.com>
2012-06-14 09:39:38 +0800

    effect

--- cms/lib-v0.3/script/effect.js
+++ cms/lib-v0.3/script/effect.js
@@ -123,54 +123,55 @@ $(document).ready(function() {
 		var data = jQuery.parseJSON(CON.attr('data'));
 		var itemWidth = parseInt(data.itemWidth) + parseInt(data.margin);
 		CON.find('.slider').css({width: data.numPerSlide * itemWidth - parseInt(data.margin)});
-		
-		var sliderUl = CON.find('.slider > ul');
-		var LIS = sliderUl.find('li');
-		var itemCount = LIS.length;
-		var totalPage = Math.ceil(itemCount/data.numPerSlide);
-		var pageToCall = 1;
-		
-		var handlers = CON.find('.handler li');
-		$(handlers[0]).addClass('active');
-		LIS.each(function(i, LI) {
-			$(LI).attr('itemNum', i);
-			if(i < data.numPerSlide) {
-				$(LI).css({position: 'absolute', left: i * itemWidth});
-				$(LI).attr('status', 'onStage');
-			} else {
-				$(LI).css({position: 'absolute', left: data.numPerSlide * itemWidth});
-				$(LI).attr('status', 'inQueue');
-			}
-		});
-		
-		var autoSlide = setInterval(function() {
-			pageToCall++;
-			//set current page to 0, meaning this is the last page
-			if(pageToCall > totalPage) { pageToCall = 1; }
-			singleFly(sliderUl, data.numPerSlide, itemWidth, itemCount, pageToCall, handlers);
-		}, data.delay);
-		CON.mouseenter(function() {
-			clearInterval(autoSlide);
-		}).mouseleave(function() {
-			autoSlide = setInterval(function() {
+		if(CON.find('.slider').attr('class') != undefined){
+			var sliderUl = CON.find('.slider > ul');
+			var LIS = sliderUl.find('li');
+			var itemCount = LIS.length;
+			var totalPage = Math.ceil(itemCount/data.numPerSlide);
+			var pageToCall = 1;
+			
+			var handlers = CON.find('.handler li');
+			$(handlers[0]).addClass('active');
+			LIS.each(function(i, LI) {
+				$(LI).attr('itemNum', i);
+				if(i < data.numPerSlide) {
+					$(LI).css({position: 'absolute', left: i * itemWidth});
+					$(LI).attr('status', 'onStage');
+				} else {
+					$(LI).css({position: 'absolute', left: data.numPerSlide * itemWidth});
+					$(LI).attr('status', 'inQueue');
+				}
+			});
+			
+			var autoSlide = setInterval(function() {
 				pageToCall++;
 				//set current page to 0, meaning this is the last page
 				if(pageToCall > totalPage) { pageToCall = 1; }
 				singleFly(sliderUl, data.numPerSlide, itemWidth, itemCount, pageToCall, handlers);
 			}, data.delay);
-		});
-		
-		//-----------------------------process handles-------------------------------//
-		$(handlers).each(function(i, handle) {
-			$(handle).click(function(e) {
-				e.preventDefault();
-				if(sliderUl.attr('status') != 'moving') {
-					pageToCall = i + 1;
+			CON.mouseenter(function() {
+				clearInterval(autoSlide);
+			}).mouseleave(function() {
+				autoSlide = setInterval(function() {
+					pageToCall++;
+					//set current page to 0, meaning this is the last page
+					if(pageToCall > totalPage) { pageToCall = 1; }
 					singleFly(sliderUl, data.numPerSlide, itemWidth, itemCount, pageToCall, handlers);
-				}
-				return false;
+				}, data.delay);
 			});
-		});
+			
+			//-----------------------------process handles-------------------------------//
+			$(handlers).each(function(i, handle) {
+				$(handle).click(function(e) {
+					e.preventDefault();
+					if(sliderUl.attr('status') != 'moving') {
+						pageToCall = i + 1;
+						singleFly(sliderUl, data.numPerSlide, itemWidth, itemCount, pageToCall, handlers);
+					}
+					return false;
+				}); 
+			});
+		}
 	});
 });
 
