2b06cd43ff72697619d7a325dc8a6caac4e46873
Gavin Li <craftgavin@gmail.com>
2013-01-17 17:14:49 +0800

    bug fixed

--- cms/v3/script/icon-selector.js
+++ cms/v3/script/icon-selector.js
@@ -65,10 +65,19 @@ var appendToCmEditor = function(data, caller) {
 	var c = codeMirrorEditor.getCursor();
 	codeMirrorEditor.replaceRange(imgTag, c);
 	codeMirrorEditor.save();
-	//console.log(c);
 }
-//append icon to ck editor
-
-
+var appendToEditor = function(data) {
+	var editor = null;
+	for(var instanceName in CKEDITOR.instances) {
+		editor = CKEDITOR.instances[instanceName];
+	}
+	
+	if ( editor.mode == 'wysiwyg' ) {
+		imgHtml = CKEDITOR.dom.element.createFromHtml("<img src='" + IconSelector.getFileServerUrl() + "/" + data.filepath + "' />");
+		editor.insertElement(imgHtml);
+	} else {
+		alert( '需要在所见即所得模式下使用!' );
+	}
+};
 
 
